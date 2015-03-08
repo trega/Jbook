651064e4e86270157a20648de98862bfcb1d11b4
trega <pan.wolodyjowski@gmail.com>
2015-03-08 23:14:42 +0100

    NotebookTree: TreeSelectionListener added

--- src/NotebookTree.java
+++ src/NotebookTree.java
@@ -2,20 +2,26 @@ import java.util.Hashtable;
 import java.util.Vector;
 
 import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreeNode;
 
 
 public class NotebookTree extends JTree {
 	private static final long serialVersionUID = 1L;
+	private NotebookTree this_tree;
 
 	public NotebookTree() {
 		super();
+		initialize();
 		fillDefaultContent();
+		
 	}
 
 	public NotebookTree(Object[] value) {
 		super(value);
+		initialize();
 		// TODO Auto-generated constructor stub
 	}
 
@@ -26,22 +32,31 @@ public class NotebookTree extends JTree {
 
 	public NotebookTree(Hashtable<?, ?> value) {
 		super(value);
+		initialize();
 		// TODO Auto-generated constructor stub
 	}
 
 	public NotebookTree(TreeNode root) {
 		super(root);
+		initialize();
 		fillDefaultContent();
 	}
 
 	public NotebookTree(TreeModel newModel) {
 		super(newModel);
+		initialize();
 		// TODO Auto-generated constructor stub
 	}
 
 	public NotebookTree(TreeNode root, boolean asksAllowsChildren) {
 		super(root, asksAllowsChildren);
+		initialize();
 		// TODO Auto-generated constructor stub
+	}
+	
+	private void initialize(){
+		this_tree = this;
+		registerEventHandlers();
 	}
 	
 	private void fillDefaultContent(){
@@ -54,6 +69,29 @@ public class NotebookTree extends JTree {
 		root.add(otherNotebook);
 		Notebook todoNotebook = new Notebook("TODO");
 		otherNotebook.add(todoNotebook);			
+	}
+	
+	private void registerEventHandlers(){
+		registerTreeSelectionListener();
+	}
+	
+	
+	
+	private void registerTreeSelectionListener(){		
+		this_tree.addTreeSelectionListener(new TreeSelectionListener() {
+		    public void valueChanged(TreeSelectionEvent e) {
+		    	Notebook node = (Notebook)this_tree.getLastSelectedPathComponent();
+		    	
+		    /* if nothing is selected */ 
+		        if (node == null) return;
+
+		    /* retrieve the node that was selected */ 
+		    //    Object nodeInfo = node.getUserObject();
+		        //...
+		    /* React to the node selection. */
+		        //...
+		    }
+		});
 	}
 
 }
